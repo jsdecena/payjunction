bc36b92e5c3ab0b4e8e5dc62eda79e118d592c2f
Jeff Simons Decena <jeff.decena@yahoo.com>
2021-07-02 14:26:01 +1200

    Add the base tests

---
+++ tests/BaseTestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Jsdecena\Payjunction\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class BaseTestCase extends TestCase
+{
+    /**
+     * Set up
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+}
---
+++ tests/Customers/CustomerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jsdecena\Payjunction\Tests\Customers;
+
+use Jsdecena\Payjunction\Tests\BaseTestCase;
+
+class CustomerTest extends BaseTestCase
+{
+    /** @test */
+    public function it_should_return_all_customers()
+    {
+
+    }
+}
